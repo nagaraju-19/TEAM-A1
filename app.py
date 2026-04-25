d37a43faef52ed90bc63f5a628a8317da0914188
nagaraju-19 <nagarajukolanupaka9@gmail.com>
2026-04-25 12:14:13 +0530

    Refactor app.run to use a variable for port

--- app.py
+++ app.py
@@ -1387,5 +1387,8 @@ def server_error(error):
 init_db()
 
 
+import os
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
